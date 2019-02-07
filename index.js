a73ecefcd829962dc9fd0f76ed2d1c904207130f
Kemal Aditya <kadityafikar@gmail.com>
2019-02-07 19:18:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const constmes = "hello im constmes"
+let letmes = "hello im letmes"
+var varmes = "hello im varmes"
+
+if (true) {
+  let letmes = 2;
+  console.log("di dalam bracket: ",letmes)
+
+}
+console.log("ini di luar :", letmes)
