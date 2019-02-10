b43da85ef0f81198a2630ab00eb33a709bbba2e9
Kemal Aditya <kadityafikar@gmail.com>
2019-02-10 10:01:04 +0700

    add script and html

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const constmes = "hello im constmes"
-let letmes = "hello im letmes"
-var varmes = "hello im varmes"
+// const constmes = "hello im constmes"
+// let letmes = "hello im letmes"
+// var varmes = "hello im varmes"
+//
+// if (true) {
+//   let letmes = 2;
+//   console.log("di dalam bracket: ",letmes)
+//
+// }
+// console.log("ini di luar :", letmes)
 
-if (true) {
-  let letmes = 2;
-  console.log("di dalam bracket: ",letmes)
+  function load() {
+    var s = document.getElementById('nomorkecepatan').value * 2
+    var message = s ? `no anda ${s}` : alert("masukan angka")
 
-}
-console.log("ini di luar :", letmes)
+    var render = document.getElementById('hasil')
+    
+    return message ? render.innerHTML = message : "no"
+  }
