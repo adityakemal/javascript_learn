f020661b0bb6dc3f8bcc19437bf64023a7bf646b
Kemal Aditya <kadityafikar@gmail.com>
2019-02-11 23:41:09 +0700

    add variable for make thats simlpy and beautify

--- js/calculator.js
+++ js/calculator.js
@@ -1,29 +1,33 @@
 // FUNCTION ENABLE/DISABLE OPERATOR
 var idOps = "#tambah,#kurang,#bagi,#kali,#equal"
-function enableOps(){
-  return $(idOps).prop("disabled",false)
+
+function enableOps() {
+  return $(idOps).prop("disabled", false)
 }
-function disableOps(){
-  return $(idOps).prop("disabled",true)
+
+function disableOps() {
+  return $(idOps).prop("disabled", true)
 }
 
 // FUNCTION ENABLE/DISABLE ANGKA
 var idAngkas = "#satu,#dua,#tiga,#empat,#lima,#enam,#tujuh,#delapan,#sembilan,#nol"
-function disableAngka(){
-  return $(idAngkas).prop("disabled",true)
+
+function disableAngka() {
+  return $(idAngkas).prop("disabled", true)
 }
-function enableAngka(){
-  return $(idAngkas).prop("disabled",false)
+
+function enableAngka() {
+  return $(idAngkas).prop("disabled", false)
 }
 
 // FUNTION INPUT NUMBER
-function inputAngka(val){
-  return $(val).click(()=>{
-            let s = $(val).val()
-            $('#hasil').append(s)
-            console.log(s);
-            enableOps()
-          })
+function inputAngka(val) {
+  return $(val).click(() => {
+    let s = $(val).val()
+    $('#hasil').append(s)
+    console.log(s);
+    enableOps()
+  })
 }
 
 inputAngka("#satu")
@@ -39,14 +43,14 @@ inputAngka("#nol")
 
 // FUNCTION OPERATOR
 function operator(op) {
-  return $(op).click(()=>{
-            let s = $(op).val()
-            $('#hasil').append(s)
-            // prop set disabled
-            disableOps()
-            enableAngka()
-            console.log(s);
-          })
+  return $(op).click(() => {
+    let s = $(op).val()
+    $('#hasil').append(s)
+    // prop set disabled
+    disableOps()
+    enableAngka()
+    console.log(s);
+  })
 }
 
 operator("#tambah")
@@ -55,7 +59,7 @@ operator("#bagi")
 operator("#kali")
 
 // jumlah total
-$('#equal').click(()=>{
+$('#equal').click(() => {
   var x = $('#hasil').text()
   // console.log(eval(x));
   // console.log(parseFloat(eval(x)));
