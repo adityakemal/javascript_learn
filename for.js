b58be3c9597b08cf601767b0b11d5513fda915ce
Kemal Aditya <kadityafikar@gmail.com>
2019-02-13 20:08:45 +0700

    add es6 for if script example

--- for.js
+++ for.js
@@ -7,3 +7,40 @@ var perulangan = ()=>{
 }
 
 perulangan()
+
+// ES 5 for each harus di SPLIT dlu jadi array
+var five = "INI ES 5 FOR EACH"
+var five = five.split("")
+function iniForEach() {
+  five.forEach(function(val) {
+    console.log("contoh for each",val);
+  })
+}
+iniForEach()
+
+//kalo for in akan membaca menjadi number per character array
+var helloIn = "hello world"
+var nesArr = [["kemal"],["adiya"],["zulfikar"]]
+function iniForIf() {
+
+  for(var h in nesArr){
+    console.log("for in :",nesArr[h])
+  }
+
+}
+iniForIf()
+
+
+// ES 6 "for of" adalah seperti penggabungan for-in dan foreach di es5
+var hello = "INI ES6 FOR-OF"
+var obj = [{nama:"kemal", alamat:"bandung", hp:0987654321},
+            {nama:"aditya", alamat:"jakarta", hp:0987654321}]
+var
+function iniForOf() {
+
+  for(var h of obj){
+    console.log(h.nama + " tinggal di " + h.alamat)
+  }
+
+}
+iniForOf()
