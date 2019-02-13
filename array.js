c4bd244811b37a58105ffcf4b0092f90b63baefd
Kemal Aditya <kadityafikar@gmail.com>
2019-02-14 02:23:51 +0700

    add array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+function panggilArray() {
+  var arrNum = [1,2,3,4,5,6];
+  var arrNama = ["kemal","aditya","zulfikar"];
+
+  console.log(arrNum.length);
+  console.log(arrNama.length);
+  console.log(arrNama[2]);
+}
+panggilArray()
