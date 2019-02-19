456a024b32accc49c16e7f1e6184f66e3ef63d11
Kemal Aditya <kadityafikar@gmail.com>
2019-02-20 03:09:12 +0700

    add shiftjs

---
+++ shift.js
@@ -0,0 +1,22 @@
+//shift spperti mengambil isi dalam array dan di tampung di shift itu sendiri sehingga dalam array akan berkurang 1, unshift() sebaliknya bisa menambah pada array
+function panggilShift() {
+  var kota = ['bandung','garut','jakarta','cirebon','jogja']
+  //console.log(kota);
+  console.log('========================');
+
+   for (var i = 0; i < 3; i++) {
+     k = kota.shift()
+     console.log(k);
+   }
+  return kota
+}
+console.log(panggilShift());
+
+function panggilUnshift() {
+  var arr = ["aaa"]
+  arr.unshift('xxx')
+  arr.unshift('yyy')
+  arr.unshift('zzz')
+return arr
+}
+console.log(panggilUnshift());
