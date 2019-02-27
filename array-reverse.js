dca2d35a88b3976738d7da1b644017098764feb2
Kemal Aditya <kadityafikar@gmail.com>
2019-02-27 20:02:48 +0700

    add modulus

---
+++ array-reverse.js
@@ -0,0 +1,7 @@
+function panggilReverse() {
+  var arr = ["bandung","jakarta","depok","bali","tasik",]
+  var result = arr.reverse()
+  return result
+}
+
+console.log(panggilReverse());
---
+++ modulus.js
@@ -0,0 +1,9 @@
+//Operator Modulus adalah sebuah operasi yang menghasilkan sisa pembagian dari suatu
+//bilangan (terhadap bilangan pembaginya). Di javascript, modulus dilambangkan dengan simbol persen (%)
+
+function modulus() {
+  var data1 = 11
+  var data2 = 2
+  return data1 % data2
+}
+console.log(modulus());
